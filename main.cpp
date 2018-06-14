5bc218a682581f3c514f1bd8aedd7780e8a5c583
William Bernardoni <wbernar5@jhu.edu>
2018-06-14 14:08:49 -0400

    Modifiable how big generated image

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <fstream>
 
+#define XPIX "100"
+#define YPIX "100"
+
 using namespace std;
 
 int main()
 {
   ofstream out;
   out.open("im.ppm");
-  out << "P3 3 3 255" << endl;
-  out << "255 255 255 255 255 255 255 255 255" << endl;
-  out << "255 255 255 255 255 255 255 255 255" << endl;
-  out << "255 255 255 255 255 255 255 255 255" << endl;
+  out << "P3 " << XPIX << " " <<  YPIX << " 255" << endl;
+  for (int x = 0; x < atoi(XPIX); x++)
+  {
+    for (int y = 0; y < atoi(YPIX); y++)
+    {
+      out << " 100 50 255 ";
+    }
+    out << endl;
+  } 
   out.close();
 }
 
